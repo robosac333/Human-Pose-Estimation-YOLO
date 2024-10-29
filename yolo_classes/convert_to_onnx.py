088bc59224161390f2a44f56a2a09ad106674e9f
Navdeep Singh <singh.nav2100@gmail.com>
2024-10-28 21:41:06 -0400

    excluding yolov3.weights to get uploaded as it is a failrly large file

--- yolo_classes/convert_to_onnx.py
+++ yolo_classes/convert_to_onnx.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 # Load the YOLO11 model
 model = YOLO("yolo_classes/yolo11x.pt")
 
-Export the model to ONNX format
+# Export the model to ONNX format
 model.export(format="onnx")  # creates 'yolo11n.onnx'
 
 # Load the exported ONNX model
